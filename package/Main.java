0cdf582f1a73eaf8144e10e597bc7ee44f47e8b3
reetha123 <reethasaji1@gmail.com>
2025-08-13 00:20:06 +0530

    package

---
+++ package/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import demo.Demo;
+
+public class Main {
+public static void main(String[] args) {
+Demo helper = new Demo();
+System.out.println(helper.greeting);
+helper.showDetails();
+}
+}
